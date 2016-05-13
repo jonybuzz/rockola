ee4d5102ae2152f7999f548ba25ad66aa13a91d5
EMiX <emix.lr@gmail.com>
2016-05-13 16:40:34 -0300

    Agregue vista login.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index');
-
 });
 
 router.get('/reproductor', function (req, res) {
     res.render('reproductor');
 });
 
-router.get('/cliente', function(req, res, next) {
-  res.render('cliente');
+router.get('/cliente', function (req, res, next) {
+    res.render('cliente');
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login');
 });
 
 module.exports = router;
