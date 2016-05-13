d6742b0cda6abe28e30ea2d42a6cbe745e1a37c4
EMiX <emix.lr@gmail.com>
2016-05-13 11:48:56 -0300

    Agregue vista de reproductor

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var databaseUrl = "my-db";
-var collections  = ["usuarios"];
-var db = require("mongojs")(databaseUrl,collections);
+var collections = ["usuarios"];
+var db = require("mongojs")(databaseUrl, collections);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  db.usuarios.save({id:1, nombre: "eduardo"});  
-  res.render('index');
-  
+router.get('/', function (req, res, next) {
+    db.usuarios.save({id: 1, nombre: "eduardo"});
+    res.render('index');
+
+});
+
+router.get('/reproductor', function (req, res) {
+    res.render('reproductor');
 });
 module.exports = router;
