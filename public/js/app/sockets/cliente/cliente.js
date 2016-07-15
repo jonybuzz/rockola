4b8742f8885e42049a5b2028de0036508073c272
Eduardo <hernandezed.1991@gmail.com>
2016-07-15 17:42:45 -0300

    Reproduciendo en cliente

--- public/js/app/sockets/cliente/cliente.js
+++ public/js/app/sockets/cliente/cliente.js
@@ -1,13 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  var socket = io();
+    var socket = io();
 
-  function actualizarListaTemas(temas) {
-    var html = $("#bodyListaTemplate").render(temas);
-    $("#body-lista-reproduccion").html(html);
-  }
+    function actualizarListaTemas(temas) {
+        var html = $("#bodyListaTemplate").render(temas);
+        $("#body-lista-reproduccion").html(html);
 
-  socket.on('actualizarLista', function(docs) {
-    actualizarListaTemas(docs);
-  });
+        $(".lista-de-temas .collection-item").first().find("p").css("color", "mediumaquamarine")
+        $(".lista-de-temas .collection-item").first().find("p").last().append("Reproduciendo")
+
+    }
+
+    socket.on('actualizarLista', function (docs) {
+        actualizarListaTemas(docs);
+    });
 });
