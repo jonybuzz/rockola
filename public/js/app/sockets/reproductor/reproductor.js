bd1f625ca5043de35c8f7c1d1466606a8ba30fa6
jonyb <jonyb@DESKTOP-JNE0F39>
2016-10-09 16:04:38 -0300

    refactor visual del reproductor

--- public/js/app/sockets/reproductor/reproductor.js
+++ public/js/app/sockets/reproductor/reproductor.js
@@ -5,9 +5,14 @@ $(document).ready(function () {
     function actualizarListaTemas(temas) {
         var html = $("#bodyListaTemplate").render(temas);
         $("#body-lista-reproductor").html(html);
-
-        $(".lista-de-temas .collection-item").first().find("p").css("color", "mediumaquamarine");
-        $(".lista-de-temas .collection-item").first().find("p").last().append("Reproduciendo");
+        $("#body-lista-reproductor").prepend('<li><a class="subheader"><i class="material-icons">queue_music</i> Listado de Temas</a></li>');
+        
+        $('.tooltipped').tooltip({delay: 30});
+        
+        var itemReproduciendo = $(".lista-de-temas .collection-item").first();
+        itemReproduciendo.css("color", "white");
+        itemReproduciendo.find("span").css("color", "purple");
+        itemReproduciendo.find("span").last().append("Reproduciendo");
 
     }
     
