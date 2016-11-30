4094ba35d63bba85df9ce68205022e52eb6e13a2
Eduardo <hernandezed.1991@gmail.com>
2016-11-30 09:42:20 -0300

    saco lineas que no se utilizaban

--- public/js/app/ui/reproductor/reproductor.js
+++ public/js/app/ui/reproductor/reproductor.js
@@ -27,27 +27,11 @@ rockola.ui.reproductor = (function () {
     }
 
     function pasarAlSiguienteTema() {
-        var nombreRockola = getCookie("rockola")
         rockola.service.tema.obtenerSiguiente()
                 .done(function (data) {
                     socket.emit("actualizame");
                     reproducir(data);
                 });
-    }
-
-    function getCookie(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
     }
 
     function onPlayerStateChange(event) {
