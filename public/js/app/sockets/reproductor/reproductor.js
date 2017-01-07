8766111d0f3a7cad7ce114a302dd96e092939a23
jonyb <jonyb@DESKTOP-JNE0F39>
2017-01-07 16:06:56 -0300

    estilos para el reproductor

--- public/js/app/sockets/reproductor/reproductor.js
+++ public/js/app/sockets/reproductor/reproductor.js
@@ -5,13 +5,14 @@ $(document).ready(function () {
     function actualizarListaTemas(temas) {
         var html = $("#bodyListaTemplate").render(temas);
         $("#body-lista-reproductor").html(html);
-        $("#body-lista-reproductor").prepend('<li><a class="subheader"><i class="material-icons">queue_music</i> Listado de Temas</a></li>');
+        $("#body-lista-reproductor").prepend('<li><h3 class="center">Lista de Temas</h3></li>');
         
         $('.tooltipped').tooltip({delay: 30});
         
         var itemReproduciendo = $(".lista-de-temas .collection-item").first();
         itemReproduciendo.css("color", "white");
         itemReproduciendo.find("span").css("color", "purple");
+        itemReproduciendo.find("span").css("font-weight", "bold");
         itemReproduciendo.find("span").last().append("Reproduciendo");
 
     }
