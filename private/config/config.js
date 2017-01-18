f2894296b79a7ecf5c99f91e19d323275b1a67fb
jonyb <jonyb@DESKTOP-JNE0F39>
2017-01-17 21:48:15 -0300

    dejo urls de prod

--- private/config/config.js
+++ private/config/config.js
@@ -1,6 +1,6 @@
 module.exports.config = {
-    //mongoUrl :"mongodb://rockola:deb0d3f9c3e6e1fc0b8792c1a10f69538256978afd7e9c95b6ca2227a8de781d@localhost:27017/rockola?authSource=admin";
-    mongoUrl: "mongodb://localhost:27017/rockola",
+    mongoUrl: "mongodb://rockola:deb0d3f9c3e6e1fc0b8792c1a10f69538256978afd7e9c95b6ca2227a8de781d@localhost:27017/rockola?authSource=admin",
+    //mongoUrl: "mongodb://localhost:27017/rockola",
     facebook: {
         appId: '169801373463876',
         secret: 'eb6e67552f004fcf25d31a91c03cbe62',
